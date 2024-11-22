584863a759a4981289a1ee1beb70a6e1e7b45aa6
israel litvak <lisrael1989@gmail.com>
2024-11-22 20:48:22 +0200

    Add footer to the rootLayout

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router';
 import { AppHeader } from './App-header/AppHeader';
+import { Footer } from './Footer/Footer';
 
 export function RootLayout() {
   return (
     <div className="main-layout">
       <AppHeader />
       <Outlet />
+      <Footer />
     </div>
   );
 }
