430201226855edd428db70e3af7984b3ccf4f2d0
israel litvak <lisrael1989@gmail.com>
2024-11-24 16:42:14 +0200

    Change logo path

--- src/components/App-header/AppHeader.jsx
+++ src/components/App-header/AppHeader.jsx
@@ -1,53 +1,3 @@
-// import { NavLink } from 'react-router-dom';
-// import { useAuth } from '../../hooks/useAuth';
-
-// export function AppHeader() {
-//   const { user, handleLogout } = useAuth();
-
-//   return (
-//     <header className="app-header">
-//       <NavLink to="/">
-//         <img className="logo" src="/public/images/logo.png" alt="logo" />
-//       </NavLink>
-//       <nav>
-//         <ul>
-//           <li>
-//             <NavLink
-//               to="/"
-//               className={({ isActive }) => (isActive ? 'active-link' : '')}
-//             >
-//               Home
-//             </NavLink>
-//           </li>
-
-//           <li>
-//             <NavLink
-//               to="/shoes"
-//               className={({ isActive }) => (isActive ? 'active-link' : '')}
-//             >
-//               Shoes
-//             </NavLink>
-//           </li>
-//           <li className="user-actions">
-//             {user ? (
-//               <>
-//                 <span className="welcome-message">Welcome, {user.name}</span>
-//                 <button className="logout-btn" onClick={handleLogout}>
-//                   Logout
-//                 </button>
-//               </>
-//             ) : (
-//               <NavLink to="/login" className="login-link">
-//                 Login
-//               </NavLink>
-//             )}
-//           </li>
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// }
-
 import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 
@@ -57,7 +7,7 @@ export function AppHeader() {
   return (
     <header className="app-header">
       <NavLink to="/">
-        <img className="logo" src="/public/images/logo.png" alt="logo" />
+        <img className="logo" src="/images/logo.png" alt="logo" />
       </NavLink>
       <nav>
         <ul>
